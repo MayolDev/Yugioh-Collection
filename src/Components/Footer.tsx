16fd7e8c794f59f2f8d1b987a9760a47c2d37f82
Iván Mayol Braza <92253706+MayolDev@users.noreply.github.com>
2024-01-13 23:18:14 +0100

    Finish Footer and app

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,6 +3,9 @@ export const Footer = () => {
     return (
         <div className="bg-[#F8E9C0] rounded w-full py-2 text-black mt-10 mb-2">
             <p>Created by <a className="text-[#580069] font-bold" href="https://www.mayoldev.es/" target="_blank">@mayoldev</a> with ❤️</p>
+            <p>API used: <a className="text-[#580069] font-bold" href="https://db.ygoprodeck.com/api-guide/" target="_blank">YGOProDeck</a></p>
+            <p>Yu-Gi-Oh! and all related materials are © 1996 Kazuki Takahashi. © 2017 NAS · TV Tokyo.</p>
+            
         </div>
     )
 }
